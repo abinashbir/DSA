f38041a3215677f41bcbe0796b25be8e477569e1
abinash bir <abinashbir13112@gmail.com>
2026-02-08 23:54:14 +0530

    age_validater problem

---
+++ basic/age_validater.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter your age :";
+    cin>>age;
+
+    if(age>=18){
+        cout<<"You are eligible to choose your representitives";
+    }else{
+        cout<<"You are not eligible to cast the vote";
+    }
+
+}
