7a3daf50debb660ad2dc47abd0e13291311ce159
abinash bir <abinashbir13112@gmail.com>
2026-02-08 11:38:37 +0530

    basic folder created

--- array/one_d_array.cpp
+++ array/one_d_array.cpp
@@ -1,11 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-
-int main(){
-    int x,y,z;
-    z = x + y;
-    cin>>x>>y;
-    cout<<"The value of x is : "<<x<<endl<<"And the value of y is : "<<y<<endl;
-    cout<<"The sum of  x and y is "<<z<<endl;
-}
---
+++ basic/in_out.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int x,y,z;
+    z = x + y;
+    cout<<"Enter the vlaue of x :";
+    cin>>x;
+    cout<<"Enter The value of y :";
+    cin>>y;
+    cout<<x+y<<endl;
+}
