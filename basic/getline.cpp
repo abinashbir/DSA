d5495fc0f696f94683dfc81f54013fea706543d0
abinash bir <abinashbir13112@gmail.com>
2026-02-08 12:00:16 +0530

    Learned getline func

---
+++ basic/getline.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s1;
+
+
+    cout<<"Enter the line : ";
+    getline(cin,s1);
+
+    cout<<"The line you enter is : "<<s1<<endl;
+
+
+}
+
