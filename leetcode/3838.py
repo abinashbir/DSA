62216e0481272408eb7148b4654278dc692d47eb
abinashbir <abinashbir13112@gmail.com>
2026-06-13 21:08:35 +0530

    leetcode streak

---
+++ leetcode/3838.py
@@ -0,0 +1,13 @@
+array=["abcd","efgh","ijkl","mnop"]
+
+weight=[x for x in range(1,27)]
+
+first_string=array[0]
+
+first_string_characters=list(first_string)
+
+print(first_string_characters)
+
+print(weight)
+
+
